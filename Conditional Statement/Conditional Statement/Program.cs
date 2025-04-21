d3883c96efa3a2b4a08a2ce4818a6c6d716a5964
Nada Mohamed <nadamosad10102002@gmail.com>
2025-04-21 21:03:32 +0200

    upload files

---
+++ Conditional Statement/Conditional Statement/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Conditional_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  conditional statement
+             *   if
+             *  
+             *  **/
+            int x = 7, y = 5;
+            if(x>y)
+                Console.WriteLine("x is greater than y");
+            else if( x< y)
+                Console.WriteLine("x is less than y");
+            else if (x == y)
+                Console.WriteLine("x is equal  y ");
+            else
+                Console.WriteLine(" error");
+
+
+            /*  Nested if **/
+            Console.WriteLine("enter First Number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter Second Number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if( x==y)
+                Console.WriteLine(" x is equal y");
+            else
+            {
+                if(x >y)
+                    Console.WriteLine("x is greater than y");
+                else if(x<y)
+                    Console.WriteLine("x is less than y");
+            }
+
+            /**  logical operator in c#
+             * AND operator  two condition must is true
+             * Or operator    one condition is true 
+             */
+
+
+
+            /** ternary operator ***/
+            int num1 = 15, num2 = 7;
+            var result = x > y ? "x is greater y" : "x is less than y";
+            Console.WriteLine(result);
+
+
+            /*  switch in c#  **/
+
+            var temp = "1";
+            //switch(temp)
+            //{
+            //    case <= 20.2:
+            //        Console.WriteLine("temp is low ");
+            //        break;
+            //    case >= 5.0:
+            //        Console.WriteLine("tem is high");
+            //        break;
+            //    deafult:
+            //        Console.WriteLine("temp is zero");
+            //        break;  
+            //}
+
+            /**   switch syntax update **/
+
+            var message = temp switch
+            {
+                "one" => "one",
+                "2" => "two",
+                "3" => "three",
+                _ => "error"
+            };
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Operators in c#/Operators in c#/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Operators_in_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Arithmetic operator */
+            int number1 = 11;
+            int number2 = 2;
+            Console.WriteLine($"addition of two nuumber {number1+number2}");
+            Console.WriteLine($"Subtraction of two number {number1 - number2}");
+            Console.WriteLine($"Division of two number {number1/ number2}");
+            Console.WriteLine($"multi of two number {number1 * number2}");
+            Console.WriteLine($"moduls of two number {number1%number2}");
+
+            /*  precedence of operator in c# 
+             *  1- ()
+             *  2- * , / , %
+             *  3- + , - 
+             *  **/
+            Console.WriteLine(2+2*2); // 6
+            Console.WriteLine((2+2)*2); // 8
+
+            Console.WriteLine("===========================");
+
+
+            /* increment and decrement in c# **/
+            /* increment 
+             *  postfix , prefix
+             */
+            // postfix increment
+            int i = 5;
+            Console.WriteLine(i++); // 5
+            Console.WriteLine(i);  //  6
+
+            // prefix increment 
+            int j = 2;
+            Console.WriteLine(++j);   // 3
+            Console.WriteLine(j);     // 3
+
+            // postfix decrement
+            int k = 5;
+            Console.WriteLine(k--);    // 5
+            Console.WriteLine(k);     //  4
+
+            // prefix decrement 
+            int m = 4;
+            Console.WriteLine(--m);  // 3
+            Console.WriteLine(m);    // 3
+
+
+            /** comparsion operator in c# 
+             * equal than ==
+             *  greater than >
+             *  greater than or equal >=
+             *  less than <
+             *  less than or equal <= 
+             *  not equal !=
+             *  * **/
+            int x = 5;
+            int y = 4;
+            Console.WriteLine(x==y);   // false 
+            Console.WriteLine(x!=y);   //  true 
+            Console.WriteLine(x<y);    // false 
+            Console.WriteLine(x<=y);   //  false
+            Console.WriteLine(x>y);    // true
+            Console.WriteLine(x>=y);   //  true
+
+
+
+            int a = 5;
+            int b = a++;
+            Console.WriteLine(b);  //  5
+            Console.WriteLine(a);  // 6
+
+
+
+
+
+        }
+    }
+}

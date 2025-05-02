f02d81aacc514ff7134bd803180dce72b63ed27e
Nada Mohamed <nadamosad10102002@gmail.com>
2025-05-02 16:54:27 +0300

    upload files

---
+++ Constants &String manipulation/Constants &String manipulation/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Constants__String_manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string manipulation  **/
+            String word = "   hello nada mohamed   ";
+            Console.WriteLine(word.Trim());
+            Console.WriteLine(word[4]);
+            Console.WriteLine(word.Trim().ToUpper());
+            Console.WriteLine(word.Trim().ToLower());
+            Console.WriteLine(word.Length);
+            Console.WriteLine(word.Replace('h','H'));
+            Console.WriteLine(word.IndexOf('a'));
+            Console.WriteLine(word.LastIndexOf('d'));
+            Console.WriteLine(word.Substring(5));
+            Console.WriteLine(word.Remove(4));
+            Console.WriteLine(word.Insert(0,"parhame"));
+
+            /* Escap sequance in c# ***/
+            Console.WriteLine(" \nada\"");
+            Console.WriteLine("c:\\");
+            Console.WriteLine("My Dear , \n from Nada to My love \n i love you very very much ");
+
+
+            /* interpolated verbatim string ***/
+            var messsage = @"parham is a beautiful ,
+                          and i lovw you very very much ";
+            Console.WriteLine(messsage);
+
+            var NewFile = @"C:\\Document\file.txt";
+            Console.WriteLine(NewFile);
+
+
+        }
+    }
+}
---
+++ Loop In c#/Loop In c#/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Loop_In_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /****  for loop **********/
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("=========================");
+
+            for (int j = 5; j > 0; j--)
+            {
+                Console.WriteLine(j);
+            }
+
+
+            /********** infinit loop *************/
+
+            //for(int i = 0; i > 0; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for(; ;  )
+            //{
+            //    Console.WriteLine("infinit loop ");
+            //}
+
+
+            /*********** while loop *********/
+
+            int c = 1;
+            while (c <= 5)
+            {
+                Console.WriteLine($"i = {c}");
+                c++;
+            }
+
+
+            /*********** do while  *******/
+
+
+            int a = 1;
+            do
+            {
+                Console.WriteLine($"a ={a}");
+                a++;
+            } while (a <= 5);
+
+
+            /************ nested loop ***************/
+
+            for (int n = 1; n <= 5; n++)
+            {
+                Console.WriteLine($"first loop {n}");
+                for (int m = 1; m <= 5; m++)
+                {
+                    Console.WriteLine($"second loop {m}");
+                }
+            }
+
+
+
+
+            /******** multiplication in table *********/
+
+            for (int f = 1; f <= 12; f++)
+            {
+                for(int e =1;e<=12;e++)
+                {
+                    Console.WriteLine($"{f}*{e}={f*e}");
+                }
+                Console.WriteLine("========================");
+            }
+
+
+        }
+    }
+
+}
+
+
+        
+
+
+
+
+
+
+
+

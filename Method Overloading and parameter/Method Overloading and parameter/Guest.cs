4ef351491e58164ded07687ba0945d9050849bbd
Nada Mohamed <nadamosad10102002@gmail.com>
2025-05-25 14:05:44 +0300

    study method overloading and optional parameter and named parameter

---
+++ Method Overloading and parameter/Method Overloading and parameter/Guest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_and_parameter
+{
+    public  class Guest
+    {
+        public  void  guestMessage()
+        {
+            Console.WriteLine("How Can I Help you");
+        }
+        public  void guestMessage(string guestName)
+        {
+            Console.WriteLine($"Good Day {guestName} , How Can I Help you");
+        }
+
+        public void DisplayPlayer(int id , string name="" , int goal=0, int age=0)
+        {
+            Console.WriteLine($"id is : {id} and name of player {name}");
+        }
+
+        public void namedparamter(int id, string name, int goal)
+        {
+            Console.WriteLine($"id is : {id} and name of player {name} and goal {goal}");
+        }
+    }
+}
---
+++ Method Overloading and parameter/Method Overloading and parameter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Method_Overloading_and_parameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guest guest = new Guest();
+
+            string guestName = "";
+            Console.WriteLine("Dear Guest , What is your Name ");
+            guestName = Console.ReadLine();
+            if(guestName == string.Empty)
+            {
+                guest.guestMessage();
+            }
+            else
+            {
+                guest.guestMessage(guestName);
+            }
+
+            guest.DisplayPlayer(1, "nada");
+
+
+            //  named parameter 
+
+            guest.namedparamter(name: "nada", id: 1, goal: 4);
+        }
+    }
+}
